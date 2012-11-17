a8c2d1461b4b8454a2572892cc27357be985eb0c
Marco Bonetti <mbonetti@gmail.com>
2012-11-17 17:11:50 +0100

    setuptools packaged

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name='pymill',
+    version='0.1',
+    description='Python API for Paymill credit card processing service',
+    packages=['pymill'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    requires=['pycurl2']
+)
